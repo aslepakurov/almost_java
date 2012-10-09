ccc4c2d243b0fa566c66ffbb7f63083473597da2
Barabashka <ericbarashkin@gmail.com>
2012-10-09 16:05:53 -0700

    Lexical start

---
+++ com/lexical/Token.java
@@ -0,0 +1,12 @@
+package com.lexical;
+public class Token {
+	public String name;
+	public String attributes;
+	public String type;
+	public Token(String name, String attributes, String type) {
+		super();
+		this.name = name;
+		this.attributes = attributes;
+		this.type = type;
+	}
+}
