89d5662e6b4a00e50cd021ff3acf1c2188352f2d
Barabashka <ericbarashkin@gmail.com>
2012-10-10 13:13:03 -0700

    test

--- Raxacoricofallapatorius/Token.java
+++ Raxacoricofallapatorius/Token.java
@@ -4,7 +4,6 @@ public class Token {
 	public String type;
 	public int line;
 	public Token(String name, String attributes, String type,int line) {
-		super();
 		this.name = name;
 		this.attributes = attributes;
 		this.type = type;
