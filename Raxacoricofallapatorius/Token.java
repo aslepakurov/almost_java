5260bb9362815d1ef22d48e8966a2e490da1456f
nOob1que <vlad.shiyka@gmail.com>
2012-10-10 22:01:39 +0300

    Added by awesome dude

--- Raxacoricofallapatorius/Token.java
+++ Raxacoricofallapatorius/Token.java
@@ -3,11 +3,13 @@ public class Token {
 	public String attributes;
 	public String type;
 	public int line;
+	public boolean uselessvar;
 	public Token(String name, String attributes, String type,int line) {
 		super();
 		this.name = name;
 		this.attributes = attributes;
 		this.type = type;
 		this.line = line;
+		
 	}
 }
