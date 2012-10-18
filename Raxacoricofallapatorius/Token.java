390d9058ca53c588ed222f3a64e1a34610b83948
Sho_0teR <Sergey.Kurilo1993@gmail.com>
2012-10-19 00:08:31 +0300

    commented semantic var; reason - not used yet

--- Raxacoricofallapatorius/Token.java
+++ Raxacoricofallapatorius/Token.java
@@ -3,15 +3,15 @@ public class Token {
 	private TokenType type;
 	private int line;
 	private int column;
-	private String semanticM;
-	public Token(String name,  TokenType type, int line, int column, String semanticM) {
+//	private String semanticM;
+	public Token(String name,  TokenType type, int line, int column) {
 		this.name = name;
 		this.type = type;
 		this.line = line;
 		this.column = column;
-		this.semanticM = semanticM;
+//		this.semanticM = semanticM;
 	}
 	public String toString(){
-		return name+" "+type+" "+line+" "+column+" "+semanticM;
+		return name+" "+type+" "+line+" "+column+" ";
 	}
 }
