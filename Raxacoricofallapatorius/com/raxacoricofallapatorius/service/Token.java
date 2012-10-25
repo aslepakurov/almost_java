e2b09809a1b2dc13788944b16e24594a51d2254b
insane <insanejdm7@gmail.com>
2012-10-25 11:51:45 +0300

    +2 methods (line and col getters)

--- Raxacoricofallapatorius/com/raxacoricofallapatorius/service/Token.java
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/service/Token.java
@@ -1,20 +1,29 @@
 package com.raxacoricofallapatorius.service;
+
 public class Token {
 	private String name;
 	private TokenType type;
 	private int line;
 	private int column;
-//	private String semanticM;
-	
-	public Token(String name,  TokenType type, int line, int column) {
+
+	// private String semanticM;
+	public Token(String name, TokenType type, int line, int column) {
 		this.name = name;
 		this.type = type;
 		this.line = line;
 		this.column = column;
-//		this.semanticM = semanticM;
+		// this.semanticM = semanticM;
 	}
-	
-	public String toString(){
-		return "Name: "+name+"; Type: "+type+"; Line: "+line+"; Colomn: "+column+";";
+
+	public int getLine() {
+		return line;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public String toString() {
+		return name + " " + type + " " + line + " " + column + " ";
 	}
 }
