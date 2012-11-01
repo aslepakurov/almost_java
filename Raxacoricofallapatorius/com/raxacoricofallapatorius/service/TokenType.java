c80c9d3046b24333345b28c22bf226b41ec9bc36
Barabashkin <ericbarashkin@gmail.com>
2012-11-01 21:40:38 +0200

    Reorganized

--- Raxacoricofallapatorius/com/raxacoricofallapatorius/service/TokenType.java
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/service/TokenType.java
@@ -1,14 +1,53 @@
 package com.raxacoricofallapatorius.service;
 
 public enum TokenType {
-	TK_ID,
-	TK_STRING,
-	TK_INT,
-	TK_FLOAT,
-	TK_K_INT,TK_K_FLOAT,TK_K_STR,TK_K_VOID,TK_K_BOOL,TK_K_TRUE,
-	TK_K_FALSE,TK_K_FUNC,TK_K_FOR,TK_K_WHILE,TK_K_IF,
-	TK_K_ELSE,TK_K_CONT,TK_K_BREAK,TK_K_CONST,TK_K_DEF,TK_K_DIV,
-	TK_S_QUOT,TK_S_END,TK_S_LEFT_PARENT,TK_S_RIGHT_PARENT,TK_S_WS,TK_S_CARRET,
-	TK_S_LEFT_BRACE,TK_S_RIGHT_BRACE,TK_S_FASTAND,TK_S_FASTOR,TK_S_NOT,
-	TK_S_MULTI,TK_S_DIVIDE,TK_S_ADD,TK_S_SUBTRACT;
+	TK_ID, 			//identifier token
+	TK_STRING,		//string token
+	TK_INT,			//int num token
+	TK_FLOAT,		//float num token
+	
+	//-------Keywords--------
+	
+	TK_K_INT,		//int
+	TK_K_FLOAT,		//float
+	TK_K_STR,		//str
+	TK_K_VOID,		//void
+	TK_K_BOOL,		//bool
+	TK_K_TRUE,		//true
+	TK_K_FALSE,		//false
+	TK_K_FUNC,		//func
+	TK_K_FOR,		//for
+	TK_K_WHILE,		//while
+	TK_K_IF,		//if
+	TK_K_ELSE,		//else
+	TK_K_CONT,		//continue
+	TK_K_BREAK,		//break
+	TK_K_CONST,		//const
+	TK_K_DEF,		//def
+	TK_K_DIV,		//div
+	
+	//--------Separators-----------
+	
+	TK_S_QUOT,			//  "
+	TK_S_LEFT_PARENT,	//  (
+	TK_S_RIGHT_PARENT,	//  )
+	TK_S_LEFT_BRACE,	//  {
+	TK_S_RIGHT_BRACE,	//  }
+	TK_S_AND,			//  &
+	TK_S_OR,			//  |
+	TK_S_MULTI,			//  *
+	TK_S_DIVIDE,		//  /
+	TK_S_ADD,			//  +
+	TK_S_SUBTRACT,		//  -
+	TK_S_GREATER,		//	>
+	TK_S_LESS,			//	<
+	
+	
+	//--------Separator chx2--------
+	
+	TK_S_NOT,			//	!=
+	TK_S_EQUAL,			//  ==
+	TK_S_EQUAL_OR_GREATER,//>=
+	TK_S_EQUAL_OR_LESS	//<=
+	;
 }
