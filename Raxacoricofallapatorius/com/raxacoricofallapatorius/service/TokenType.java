07a955d9dd873a65dac3e2428014e660de2c6cd5
insane <insanejdm7@gmail.com>
2012-11-08 23:22:51 +0200

    getName method added for all types

--- Raxacoricofallapatorius/com/raxacoricofallapatorius/service/TokenType.java
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/service/TokenType.java
@@ -1,53 +1,244 @@
 package com.raxacoricofallapatorius.service;
 
 public enum TokenType {
-	TK_ID, 			//identifier token
-	TK_STRING,		//string token
-	TK_INT,			//int num token
-	TK_FLOAT,		//float num token
-	
-	//-------Keywords--------
-	
-	TK_K_INT,		//int
-	TK_K_FLOAT,		//float
-	TK_K_STR,		//str
-	TK_K_VOID,		//void
-	TK_K_BOOL,		//bool
-	TK_K_TRUE,		//true
-	TK_K_FALSE,		//false
-	TK_K_FUNC,		//func
-	TK_K_FOR,		//for
-	TK_K_WHILE,		//while
-	TK_K_IF,		//if
-	TK_K_ELSE,		//else
-	TK_K_CONTINUE,		//continue
-	TK_K_BREAK,		//break
-	TK_K_CONST,		//const
-	TK_K_DEF,		//def
-	TK_K_DIV,		//div
-	
-	//--------Separators-----------
-	
-	TK_S_QUOT,			//  "
-	TK_S_LEFT_PARENT,	//  (
-	TK_S_RIGHT_PARENT,	//  )
-	TK_S_LEFT_BRACE,	//  {
-	TK_S_RIGHT_BRACE,	//  }
-	TK_S_AND,			//  &
-	TK_S_OR,			//  |
-	TK_S_MULTI,			//  *
-	TK_S_DIVIDE,		//  /
-	TK_S_ADD,			//  +
-	TK_S_SUBTRACT,		//  -
-	TK_S_GREATER,		//	>
-	TK_S_LESS,			//	<
-	
-	
-	//--------Separator chx2--------
-	
-	TK_S_NOT,			//	!=
-	TK_S_EQUAL,			//  ==
-	TK_S_EQUAL_OR_GREATER,//>=
-	TK_S_EQUAL_OR_LESS	//<=
+	TK_ID {
+		@Override
+		public String getName() {
+			return null;
+		}
+	}, // identifier token
+	TK_STRING {
+		@Override
+		public String getName() {
+			return null;
+		}
+	}, // string token
+	TK_INT {
+		@Override
+		public String getName() {
+			return null;
+		}
+	}, // int num token
+	TK_FLOAT {
+		@Override
+		public String getName() {
+			return null;
+		}
+	}, // float num token
+
+	// -------Keywords--------
+
+	TK_K_INT {
+		@Override
+		public String getName() {
+			return "int";
+		}
+	}, // int
+	TK_K_FLOAT {
+		@Override
+		public String getName() {
+			return "float";
+		}
+	}, // float
+	TK_K_STR {
+		@Override
+		public String getName() {
+			return "str";
+		}
+	}, // str
+	TK_K_VOID {
+		@Override
+		public String getName() {
+			return "void";
+		}
+	}, // void
+	TK_K_BOOL {
+		@Override
+		public String getName() {
+			return "bool";
+		}
+	}, // bool
+	TK_K_TRUE {
+		@Override
+		public String getName() {
+			return "true";
+		}
+	}, // true
+	TK_K_FALSE {
+		@Override
+		public String getName() {
+			return "false";
+		}
+	}, // false
+	TK_K_FUNC {
+		@Override
+		public String getName() {
+			return "funk";
+		}
+	}, // func
+	TK_K_FOR {
+		@Override
+		public String getName() {
+			return "for";
+		}
+	}, // for
+	TK_K_WHILE {
+		@Override
+		public String getName() {
+			return "white";
+		}
+	}, // while
+	TK_K_IF {
+		@Override
+		public String getName() {
+			return "if";
+		}
+	}, // if
+	TK_K_ELSE {
+		@Override
+		public String getName() {
+			return "else";
+		}
+	}, // else
+	TK_K_CONTINUE {
+		@Override
+		public String getName() {
+			return "continue";
+		}
+	}, // continue
+	TK_K_BREAK {
+		@Override
+		public String getName() {
+			return "break";
+		}
+	}, // break
+	TK_K_CONST {
+		@Override
+		public String getName() {
+			return "const";
+		}
+	}, // const
+	TK_K_DEF {
+		@Override
+		public String getName() {
+			return "def";
+		}
+	}, // def
+	TK_K_DIV {
+		@Override
+		public String getName() {
+			return "div";
+		}
+	}, // div
+
+	// --------Separators-----------
+
+	TK_S_QUOT {
+		@Override
+		public String getName() {
+			return "\"";
+		}
+	}, // "
+	TK_S_LEFT_PARENT {
+		public String getName() {
+			return "(";
+		}
+	}, // (
+		// }
+	TK_S_RIGHT_PARENT {
+		@Override
+		public String getName() {
+			return ")";
+		}
+	}, // )
+	TK_S_LEFT_BRACE {
+		@Override
+		public String getName() {
+			return "{";
+		}
+	}, // {
+	TK_S_RIGHT_BRACE {
+		@Override
+		public String getName() {
+			return "}";
+		}
+	}, // }
+	TK_S_AND {
+		@Override
+		public String getName() {
+			return "&";
+		}
+	}, // &
+	TK_S_OR {
+		@Override
+		public String getName() {
+			return "|";
+		}
+	}, // |
+	TK_S_MULTI {
+		@Override
+		public String getName() {
+			return "*";
+		}
+	}, // *
+	TK_S_DIVIDE {
+		@Override
+		public String getName() {
+			return "/";
+		}
+	}, // /
+	TK_S_ADD {
+		@Override
+		public String getName() {
+			return "+";
+		}
+	}, // +
+	TK_S_SUBTRACT {
+		@Override
+		public String getName() {
+			return "-";
+		}
+	}, // -
+	TK_S_GREATER {
+		@Override
+		public String getName() {
+			return ">";
+		}
+	}, // >
+	TK_S_LESS {
+		@Override
+		public String getName() {
+			return "<";
+		}
+	}, // <
+
+	// --------Separator chx2--------
+
+	TK_S_NOT {
+		@Override
+		public String getName() {
+			return "!=";
+		}
+	}, // !=
+	TK_S_EQUAL {
+		@Override
+		public String getName() {
+			return "==";
+		}
+	}, // ==
+	TK_S_EQUAL_OR_GREATER {
+		@Override
+		public String getName() {
+			return ">=";
+		}
+	}, // >=
+	TK_S_EQUAL_OR_LESS {
+		@Override
+		public String getName() {
+			return "<=";
+		}
+	} // <=
 	;
+
+	abstract public String getName();
 }
