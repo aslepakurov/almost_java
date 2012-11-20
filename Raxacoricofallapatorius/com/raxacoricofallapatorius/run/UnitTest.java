b90a6a67edc710592ab331a9e964509050fa2faf
Barabashka <ericbarashkin@gmail.com>
2012-11-20 15:12:21 +0200

    New Class 6 unit tests : int parse double(float) parse keywords parse separators parse test real code parse

---
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/run/UnitTest.java
@@ -0,0 +1,87 @@
+package com.raxacoricofallapatorius.run;
+
+import java.util.ArrayList;
+
+import com.raxacoricofallapatorius.analyzer.LexicalAnalyzer;
+import com.raxacoricofallapatorius.service.LexerException;
+import com.raxacoricofallapatorius.service.Token;
+import com.raxacoricofallapatorius.service.TokenType;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class UnitTest extends TestCase {
+	private  ArrayList<Token> test;
+	private ArrayList<Token> expected;
+	private String code;
+	private LexicalAnalyzer la;
+	public UnitTest(String name,ArrayList<Token> expected,String code) {
+		super(name);
+		this.expected = expected;
+		this.code = code;
+	}
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		la = new LexicalAnalyzer(code);
+		test = new ArrayList<Token>();
+		Token token;
+		try {
+			do {
+				token = la.lex();
+//				System.out.println("token = " + token);
+				test.add(token);
+			} while (token.getType() != TokenType.TK_EOP);
+		} catch (LexerException e) {
+			System.out.println("Error : " + e);
+		}
+	}
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		test = null;
+	}
+	public void testInt(){
+//		System.out.println(expected);
+//		System.out.println(test);
+		assertTrue(Token.arrEquals(test, expected));
+	}
+	
+	public void testDouble(){
+//		System.out.println(expected);
+//		System.out.println(test);
+		assertTrue(Token.arrEquals(test, expected));
+	}
+	public void testString(){
+//		System.out.println(expected);
+//		System.out.println(test);
+		assertTrue(Token.arrEquals(test, expected));
+	}
+	
+	public void testKeyword(){
+//		System.out.println(expected);
+//		System.out.println(test);
+		assertTrue(Token.arrEquals(test, expected));
+	}
+	public void testSeparator(){
+//		System.out.println(expected);
+//		System.out.println(test);
+		assertTrue(Token.arrEquals(test, expected));
+	}
+	public void testVenigret(){
+//		System.out.println(expected);
+//		System.out.println(test);
+		assertTrue(Token.arrEquals(test, expected));
+	}
+	public static Test suite(){
+		TestSuite ts = new TestSuite();
+		ts.addTest(new UnitTest("testInt", TestExpected.expectedInt, TestExpected.codeInt));
+		ts.addTest(new UnitTest("testDouble", TestExpected.expectedDouble, TestExpected.codeDouble));
+		ts.addTest(new UnitTest("testString", TestExpected.expectedStrings, TestExpected.codeStrings));
+		ts.addTest(new UnitTest("testKeyword", TestExpected.expectedKeywords, TestExpected.codeKeywords));
+		ts.addTest(new UnitTest("testSeparator", TestExpected.expectedSeparator, TestExpected.codeSeparator));
+		ts.addTest(new UnitTest("testVenigret", TestExpected.expectedVenigret, TestExpected.codeVenigret));
+		return ts;
+	}
+}
