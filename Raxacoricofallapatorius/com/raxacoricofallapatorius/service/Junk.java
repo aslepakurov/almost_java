b95e69d23460b70874a8ffa6e61fe35f3ef153d7
Barabashka <ericbarashkin@gmail.com>
2012-11-28 17:07:48 +0200

    Lot of stuff was put in here deprecated class

--- Raxacoricofallapatorius/com/raxacoricofallapatorius/service/Junk.java
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/service/Junk.java
@@ -1,7 +1,5 @@
 package com.raxacoricofallapatorius.service;
-
-
-public class Junk {
+public class Junk {}
 //	/**
 //	 * Token getter
 //	 * 
@@ -136,4 +134,64 @@ public class Junk {
 	// }
 	// tokens.add(token);
 	// }
-}
+//}
+////LocalVarStatement localVarStatement = new LocalVarStatement();
+////if (look().getType() == TokenType.TK_K_DEF) {
+////	consume();
+////	if (look().getType() != TokenType.TK_S_LEFT_BRACE)
+////		throw new SyntaxException("'{' expected at " + look().getLine()
+////				+ ":" + look().getColumn());
+////	consume();
+////	localVarStatement = parseLocalVariables();
+////}
+//
+//
+///**
+// * parse function's local variable declarations method
+// * 
+// * @return object of function's local declarations class
+// * @throws SyntaxException
+// */
+//private LocalVarStatement parseLocalVariables() throws SyntaxException {
+//	ArrayList<Token> types = new ArrayList<Token>();
+//	ArrayList<Token> ids = new ArrayList<Token>();
+//	ArrayList<Token> initValues = new ArrayList<Token>();
+//	TokenType type;
+//	while (true) {
+//		if ((type = look().getType()) == TokenType.TK_S_RIGHT_BRACE) {
+//			Token[] tokens = new Token[types.size()];
+//			return new LocalVarStatement(types.toArray(tokens),
+//					ids.toArray(tokens), initValues.toArray(tokens));
+//		}
+//		if (type != TokenType.TK_K_INT || type != TokenType.TK_K_FLOAT
+//				|| type != TokenType.TK_K_STR
+//				|| type != TokenType.TK_K_BOOL)
+//			throw new SyntaxException("Variable type expected at "
+//					+ look().getLine() + ":" + look().getColumn());
+//		types.add(look());
+//		consume();
+//		if (look().getType() != TokenType.TK_ID)
+//			throw new SyntaxException("Variable id expected at "
+//					+ look().getLine() + ":" + look().getColumn());
+//		ids.add(look());
+//		consume();
+//		if (look().getType() == TokenType.TK_S_SEMICOLON) {
+//			consume();
+//			continue;
+//		}
+//		if (look().getType() != TokenType.TK_S_INIT)
+//			throw new SyntaxException("';' expected at " + look().getLine()
+//					+ ":" + look().getColumn());
+//		consume();
+//		type = look().getType();
+//		if (type != TokenType.TK_STRING || type != TokenType.TK_INT
+//				|| type != TokenType.TK_FLOAT
+//				|| type != TokenType.TK_K_TRUE
+//				|| type != TokenType.TK_K_FALSE)
+//			throw new SyntaxException(
+//					"Variable initialization expected at "
+//							+ look().getLine() + ":" + look().getColumn());
+//		initValues.add(look());
+//	}
+//}
+//}
