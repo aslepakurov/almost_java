afa8a64ddc50f9a174dce4b529eb5f939410dd9b
Barabashka <ericbarashkin@gmail.com>
2012-12-03 21:28:26 +0200

    No big changes check up

--- Raxacoricofallapatorius/com/raxacoricofallapatorius/service/Junk.java
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/service/Junk.java
@@ -195,3 +195,46 @@ public class Junk {}
 //	}
 //}
 //}
+//System.out.println("~~~~~~~~~~~~~~~~first test~~~~~~~~~~~~~~~~");
+//LexicalAnalyzer la = new LexicalAnalyzer(
+//"p for drew while serega true vlad\nawesome(var{break else paper if somethin}mouse/int 4 float 4.5 str \"sss\" pampam)");
+//ArrayList<Token> tokenlist = null;
+//try {
+//la.parse();
+//tokenlist = la.getTokens();
+//for (Token token : tokenlist) {
+//System.out.println(token.toString());
+//}
+//} catch (ParseException e) {
+//System.out.println(e);
+//}
+//
+//System.out.println("~~~~~~~~~~~~~~~~second test~~~~~~~~~~~~~~~~");
+//la = new LexicalAnalyzer(
+//"void\"123 if\" \"true_\"\"breakdown\" \nmake total insane \"\"\n void \"\"");
+//try {
+//la.parse();
+//tokenlist = la.getTokens();
+//for (Token token : tokenlist) {
+//System.out.println(token.toString());
+//}
+//} catch (ParseException e) {
+//System.out.println(e);
+//}
+//
+//System.out.println("------------------------");
+//
+//
+//System.out.println("~~~~~~~~~~~~~~~~Third test~~~~~~~~~~~~~~~~");
+//la = new LexicalAnalyzer("int 42 float 3.14 tt 13\n");
+//try {
+//la.parse();
+//tokenlist = la.getTokens();
+//for (Token token : tokenlist) {
+//System.out.println(token.toString());
+//}
+//} catch (ParseException e) {
+//System.out.println(e);
+//}
+//}
+//}
