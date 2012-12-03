484d5f6dd5ac782df734d7385d9a0263d3a7d1d6
Barabashka <ericbarashkin@gmail.com>
2012-12-03 21:28:04 +0200

    Nothing big

--- Raxacoricofallapatorius/com/raxacoricofallapatorius/service/statements/Block.java
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/service/statements/Block.java
@@ -1,6 +1,15 @@
 package com.raxacoricofallapatorius.service.statements;
 public class Block {
-	public Block() {
-		
+	private Statement[] statements;
+	private Variable[] localVar;
+	public Block(Statement[] statements, Variable[] localVar) {
+		this.statements = statements;
+		this.localVar = localVar;
+	}
+	public Statement[] getStatements() {
+		return statements;
+	}
+	public Variable[] getLocalVar() {
+		return localVar;
 	}
 }
--- Raxacoricofallapatorius/com/raxacoricofallapatorius/service/statements/Statement.java
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/service/statements/Statement.java
@@ -1,17 +1,12 @@
 package com.raxacoricofallapatorius.service.statements;
 
-import java.util.ArrayList;
 
 public class Statement {
-//	private ArrayList<Statement> statements;
-	Block block;
+	protected Block block;
 	public Statement() {
 		block = null;
-//		statements = new ArrayList<Statement>();
 	}
-//	public void addStatment() {
-//		
-//	}
+
 	public void setBlock(Block block){
 		this.block = block;
 	}
