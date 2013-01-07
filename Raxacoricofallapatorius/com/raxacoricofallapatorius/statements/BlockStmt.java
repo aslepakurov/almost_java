af412c869649e76f03b1d2fcfbe1be908deebeec
Barabashka <ericbarashkin@gmail.com>
2013-01-07 03:06:38 +0200

    Reconst

--- Raxacoricofallapatorius/com/raxacoricofallapatorius/statements/BlockStmt.java
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/statements/BlockStmt.java
@@ -2,16 +2,20 @@ package com.raxacoricofallapatorius.statements;
 
 import java.util.ArrayList;
 
+import com.raxacoricofallapatorius.service.VarDecl;
+
 
 public class BlockStmt {
 	private ArrayList<Statement> statements;
-	public BlockStmt() {
-		this.statements = new ArrayList<Statement>();
-	}
-	public void addStatement(Statement stmt){
-		statements.add(stmt);
+	private ArrayList<VarDecl> decls;
+	public BlockStmt(ArrayList<Statement> statements, ArrayList<VarDecl> decls) {
+		this.statements = statements;
+		this.decls = decls;
 	}
 	public ArrayList<Statement> getStatements() {
 		return statements;
+	}
+	public ArrayList<VarDecl> getDecls(){
+		return decls;
 	}
 }
