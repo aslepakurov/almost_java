86c1c290d69da6bb35935760c64911a524f0f771
Barabashka <ericbarashkin@gmail.com>
2013-01-11 23:42:54 +0200

    Setting parameters in constructor Default structure

--- Raxacoricofallapatorius/com/raxacoricofallapatorius/service/VarDecl.java
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/service/VarDecl.java
@@ -3,8 +3,6 @@ package com.raxacoricofallapatorius.service;
 public class VarDecl {
 	private TokenType varType;
 	private String name;
-	public VarDecl(){
-	}
 	public VarDecl(String name,TokenType type){
 		this.name = name;
 		this.varType = type;
