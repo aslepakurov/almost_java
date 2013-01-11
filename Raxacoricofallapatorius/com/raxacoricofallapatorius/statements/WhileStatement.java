260fef263fe448ad50b56e3f7d1790b6528ae628
Barabashka <ericbarashkin@gmail.com>
2013-01-11 23:44:01 +0200

    Moved variable setting -> constructor Default structure

--- Raxacoricofallapatorius/com/raxacoricofallapatorius/statements/WhileStatement.java
+++ Raxacoricofallapatorius/com/raxacoricofallapatorius/statements/WhileStatement.java
@@ -1,10 +1,18 @@
 package com.raxacoricofallapatorius.statements;
 
+import com.raxacoricofallapatorius.expression.Expression;
+
 public class WhileStatement extends Statement {
+	private Expression expr;
 	private BlockStmt block;
-	public WhileStatement() {
-	}
-	public void addBlock(BlockStmt block) {
+	public WhileStatement(Expression expr, BlockStmt block) {
+		this.expr = expr;
 		this.block = block;
+	}
+	public Expression getExpr() {
+		return expr;
+	}
+	public BlockStmt getBlock() {
+		return block;
 	}
 }
